407099cbc42e5fd21b0456e823668971e4609aa2
wojciechguze2 <wojciechguze2@gmail.com>
2023-09-04 15:03:52 +0200

    wheel of fortune styling, fixed winning choice pos, added winning effects

--- src/components/HomepageSpecial.js
+++ src/components/HomepageSpecial.js
@@ -5,7 +5,9 @@ const HomepageSpecial = () => {
         <div className="homepage-special">
             <WheelOfFortune
                 containerClass={'m-auto d-inline-flex bg-dark my-5'}
-                contentClass={'bg-dark'}
+                contentClass={'custom-bg-primary shadow-lg fs-6 fw-bold border border-light border-2'}
+                winChoiceClass={'text-warning blink-text fw-bolder'}
+                winIndicatorClass={'active'}
             />
         </div>
     )
--- src/components/WheelOfFortune.js
+++ src/components/WheelOfFortune.js
@@ -6,37 +6,65 @@ class WheelOfFortune extends Component {
         super(props);
 
         this.state = {
-            result: null,
+            result: {},
+            isFakeSpinning: false,
             isSpinning: false,
-            gameId: 1
+            gameId: 1,
+            choicesData: [],
+            startAngle: 0,
+            finalAngle: 0,
+            contentAngle: 0,
+            circleAngle: 360,
+            fakeSpinDelay: 900,
+            spinDelay: 4950
         };
     }
 
     async componentDidMount() {
-        const choices = await this.getChoicesData(this.state.gameId);
-        this.setState({ choices });
+        await this.setChoicesData(this.state.gameId)
+
+        document.documentElement.style.setProperty('--startAngle', `${this.state.startAngle}deg`);
+        document.documentElement.style.setProperty('--finalAngle', `${this.state.finalAngle}deg`);
     }
 
-    getChoicesData = async (gameId) => {
+    setChoicesData = async (gameId) => {
         const url = `${process.env.REACT_APP_BACKEND_URL}/api/games/${gameId}`
         const response = await fetch(url);
 
         const gameData = await response.json();
-        const choices = gameData['GameValues']
+        const choicesData = gameData['GameValues']
 
+        this.setState({ choicesData })
+    }
+
+    isGamePlayed = () => {
+        return (
+            !this.state.isSpinning
+            && !this.state.isFakeSpinning
+            && this.state.result
+            && this.state.result.id
+        )
+    }
+
+    isWinningChoice = (choiceId) => {
+        return !this.state.isSpinning && this.state.result && this.state.result.id === choiceId
+    }
+
+    getChoices = () => {
+        const { winChoiceClass, winChoiceTextAddition } = this.props
         let currentAngle = 0;
 
-        return choices.map((choice, index) => {
+        return this.state.choicesData.map((choice, index) => {
             const rotate = `rotate(${currentAngle}deg)`;
-            currentAngle += 360 / choices.length;
+            currentAngle += this.state.circleAngle / this.state.choicesData.length;
 
             return (
                 <div
                     key={index}
-                    className="wheel-of-fortune--choice"
-                    style={{ transform: rotate, backgroundColor: 'red' }} // todo: choice.color
+                    className={`wheel-of-fortune--choice ${this.isWinningChoice(choice.id) ? winChoiceClass : ''}`}
+                    style={{ transform: rotate }}
                 >
-                    {choice.value} {choice.Currency.name}
+                    {choice.value} {choice.Currency.name} {this.isWinningChoice(choice.id) ? winChoiceTextAddition : ''}
                 </div>
             );
         });
@@ -49,35 +77,65 @@ class WheelOfFortune extends Component {
         return await response.json();
     };
 
+    fakeSpinWheel = async () => {
+        this.setState({ isFakeSpinning: true, result: {} });
+
+        await new Promise(resolve => setTimeout(resolve, this.state.fakeSpinDelay));
+
+        await this.spinWheel();
+    }
+
     spinWheel = async () => {
-        this.setState({ isSpinning: true });
+        this.setState({ isFakeSpinning: false, isSpinning: true });
 
-        try {
-            const result = await this.getRandomGameResult(this.state.gameId);
+        const result = await this.getRandomGameResult(this.state.gameId);
+        const totalChoices = this.state.choicesData.length;
+        const resultIndex = this.state.choicesData.findIndex(choice => choice.id === result.id) + 1;
 
-            setTimeout(() => {
-                this.setState({ result: result, isSpinning: false });
-            }, 10000) // todo: fix animation and add result info
-        } catch (error) {
-            console.error(error);
-        }
+        const finalAngle = (
+            this.state.circleAngle * 10 // for prettier animation
+            - (this.state.circleAngle / totalChoices) * resultIndex  // it's winning choice angle
+            + (this.state.circleAngle / totalChoices)  // it's single choice angle
+        );
+
+        this.setState({result, finalAngle})
+        document.documentElement.style.setProperty('--startAngle', `0deg`);
+        document.documentElement.style.setProperty('--finalAngle', `${finalAngle}deg`);
+
+        await new Promise(resolve => setTimeout(resolve, this.state.spinDelay));
+
+        await this.setWheelResult(finalAngle)
     };
 
+    setWheelResult = async (finalAngle) => {
+        this.setState({ contentAngle: finalAngle, isSpinning: false });
+        document.documentElement.style.setProperty('--contentAngle', `${finalAngle}deg`);
+    }
+
     render() {
-        const { containerClass, contentClass } = this.props;
-        const { choices, isSpinning } = this.state;
+        const { containerClass, contentClass, winIndicatorClass } = this.props;
+        const { isSpinning, isFakeSpinning } = this.state;
 
         return (
             <div>
-                <div className={`wheel-of-fortune ${containerClass}`}>
-                    <div className={`wheel-of-fortune--content ${isSpinning ? 'spin' : ''} ${contentClass}`}>
-                        {choices}
+                <div className="wheel-of-fortune-container">
+                    <div
+                        className={`wheel-of-fortune-win-indicator my-auto d-inline-flex ${this.isGamePlayed() ? winIndicatorClass : '' }`}
+                    />
+                    <div className={`wheel-of-fortune ${containerClass}`}>
+                        <div
+                            className={`wheel-of-fortune--content ${isSpinning ? 'spin' : ''} ${isFakeSpinning ? 'fake-spin' : ''} ${contentClass}`}
+                            style={{transform: `rotate(${this.state.contentAngle}deg)`}}
+                        >
+                            {this.getChoices()}
+                        </div>
                     </div>
                 </div>
                 <div className="mt-auto mb-5">
                     <button
-                        className="btn btn-warning btn-lg w-50 text-dark"
-                        onClick={this.spinWheel}
+                        className={`btn btn-warning btn-lg w-50 text-dark ${isSpinning || isFakeSpinning ? 'disabled' : ''}`}
+                        onClick={this.fakeSpinWheel}
+                        disabled={isSpinning}
                     >
                         Zagraj
                     </button>
